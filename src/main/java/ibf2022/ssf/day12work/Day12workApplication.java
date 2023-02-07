a21b40b9c2444576c703109ff4db7016f1c7d145
aneethajude <aneetha.joseph@gmail.com>
2023-02-07 10:04:11 +0800

    first day12 work commit

---
+++ src/main/java/ibf2022/ssf/day12work/Day12workApplication.java
@@ -0,0 +1,13 @@
+package ibf2022.ssf.day12work;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Day12workApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Day12workApplication.class, args);
+	}
+
+}
